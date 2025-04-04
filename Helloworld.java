207c2d8b5a3d1d2f597055d3187af1ef709cd1df
Vikash18224 <e.vikash18@gmail.com>
2025-04-05 00:18:32 +0530

    Initial Commit

---
+++ Helloworld.java
@@ -0,0 +1,7 @@
+public class Helloworld {
+    public static void main(String[] args) {
+        System.out.println("Hi Vijay");
+        System.out.println("Hi Vikash");
+        System.out.println("Hi Virat");
+    }
+}
